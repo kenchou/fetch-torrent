88acac00999b8534d1709a72918590e23bb2652c
Ken Chou <zhang-j@klab.com>
2017-12-12 11:07:42 +0800

    support token

--- add-bt-task.py
+++ add-bt-task.py
@@ -9,6 +9,9 @@ import logging
 import os.path
 import requests
 
+
+logging.basicConfig(level=logging.DEBUG)
+
 logger = logging.getLogger(__name__)
 click_log.basic_config(logger)
 
@@ -23,13 +26,27 @@ def main(torrent_file):
     with open(config_file) as f:
         config = json.load(f)
     aria2_rpc_url = config.get('aria2-rpc-url', 'http://localhost:6800/jsonrpc')
+    token = config.get('token')
+
+    params = []
+    if token:
+        params.append('token:{}'.format(token))
 
     with open(torrent_file, 'rb') as f:
-        torrent = base64.b64encode(f.read())
+        torrent = base64.b64encode(f.read()).decode('utf-8')
+        params.append(torrent)
 
-    task_id = hashlib.sha1(torrent_file)
-    data = json.dumps({'jsonrpc': '2.0', 'id': task_id, 'method': 'aria2.addTorrent', 'params': [torrent]})
-    requests.get(aria2_rpc_url, data)
+    task_id = hashlib.sha1(os.path.basename(torrent_file).encode('utf-8')).hexdigest()
+
+    data = json.dumps({'jsonrpc': '2.0', 'id': task_id, 'method': 'aria2.addTorrent', 'params': params})
+
+    # print(aria2_rpc_url, torrent_file)
+
+    headers = {'content-type': 'application/json'}
+
+    response = requests.post(aria2_rpc_url, data=data, headers=headers)
+
+    print(response.json())
 
 
 if __name__ == '__main__':
