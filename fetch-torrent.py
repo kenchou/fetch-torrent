86afa75ae1f7d760e38601d8bf26242ea07d0ace
Ken Chou <zhang-j@klab.com>
2017-12-22 09:33:28 +0800

    support proxy

--- fetch-torrent.py
+++ fetch-torrent.py
@@ -11,33 +11,36 @@ import rfc6266
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+
+LOGGING_LEVELS = {
+    0: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG
+}
+
+
 logger = logging.getLogger(__name__)
 click_log.basic_config(logger)
 
 
 @click.command()
 @click.argument('url')
-@click.option('-v', '--verbose', count=True, help='set log level.\n'
-                                                  '-v=warning\n'
-                                                  '-vv=info,\n'
-                                                  '-vvv=debug,\n'
-                                                  '-vvvv=show http debug')
-def mdt(url, verbose):
+@click.option('--proxy', help='proxy. example: socks5://user:pass@host:port')
+@click.option('-v', '--verbose', count=True, help='set log level.')
+def mdt(url, proxy, verbose):
     """LoveLive! Master Data Tools."""
+    logging.basicConfig(level=LOGGING_LEVELS.get(verbose, logging.DEBUG))
     logger.info(url)
 
-    # parsed_url = urlparse(url)
-    # parsed_query_string = parse_qs(parsed_url.query)
-    # print(parsed_url, parsed_query_string)
-    #
-    # post_url = urljoin(url, 'fetch.php')
-    # print(post_url)
-    #
-    # r = requests.post(post_url, {'code'})
-    #
-    # exit()
+    if proxy:
+        proxies = {
+            'http': proxy,
+            'https': proxy,
+        }
+    else:
+        proxies = {}
 
-    r = requests.get(url)
+    r = requests.get(url, proxies=proxies)
 
     print('Code    :', r.status_code)
     print('Encoding:', r.encoding)
@@ -50,7 +53,7 @@ def mdt(url, verbose):
     print(data)
 
     print('=======')
-    r = requests.post(urljoin(url, doc.form['action']), data=data)
+    r = requests.post(urljoin(url, doc.form['action']), data=data, proxies=proxies)
     print(r)
     print(r.headers)
     filename = rfc6266.parse_headers(r.headers['Content-Disposition']).filename_unsafe
